d8bcb3ff31216efacad16d476834f724ccdbeedf
Sébastien GELDREICH <trehinos@gmail.com>
2022-08-02 01:16:10 +0200

    Tests/arithmetic : changed program to test 64 bits operations.

--- Tests/arithmetic.cpp
+++ Tests/arithmetic.cpp
@@ -4,11 +4,11 @@
 std::array<Juse::U8, 256> Juse::Tests::arithmetic()
 {
     return {
-        0x14, 0x00, 0, 0, 1, // Words[0] = 1
-        0x14, 0x00, 1, 0x27, 0x0f, // Words[1] = 9999
-        0x15, 0x00, 2, 0, 1, 255, // Words[2] = Words[0] + Words[1] CR Words[255]
-        0x17, 0x00, 2, // out Words[2]
-        0x17, 0x00, 255, // out Words[255]
+        0x1C, 0x00, 0, 0, 0, 1, 1, 1, 1, 1, 1, // Longs[0] = 1
+        0x1C, 0x00, 1, 0, 0, 1, 1, 1, 1, 1, 1, // Longs[1] = 9999
+        0x1D, 0x00, 2, 0, 1, 255, // Longs[2] = Longs[0] + Longs[1] CR Longs[255]
+        0x1F, 0x00, 2, // out Longs[2]
+        0x1F, 0x00, 255, // out Longs[255]
         0x00, 0x0F // end
     };
 }
