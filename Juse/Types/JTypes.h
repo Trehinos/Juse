3456f9f623a622873cb706a3c8dab4ba0f07547b
Sébastien GELDREICH <trehinos@gmail.com>
2022-08-20 18:24:26 +0200

    Jtypes some updates

--- Juse/Types/JTypes.h
+++ Juse/Types/JTypes.h
@@ -3,53 +3,69 @@
 #include "../types.h"
 #include "../utility.h"
 
-namespace Juse {
-    namespace Types {
-        struct Model {
+namespace Juse
+{
+    namespace Types
+    {
+        struct Model
+        {
             S8 name;
-            inline virtual ByteSet toSet()
-            {
-                return ByteSet{};
-            }
+            inline virtual S8 toString() { return "{" + name + "}"; }
+            inline virtual ByteSet toSet() { return ByteSet {}; }
             inline virtual void fromSet(ByteSet)
             {
             }
+            inline virtual U16 size() { return U16(toSet().size()); }
             Model(S8 name = "") : name(name) {}
         };
 
-        const struct Nil : public Model {
-            Nil() : Model("nil") {}
-        } nil;
-
-        struct Null : public virtual Model {
-            Null() : Model("null") {}
-            inline ByteSet toSet()
-            {
-                return ByteSet{ 0 };
-            }
-        };
-
         using ModelMap = HeapMap<S8, Model>;
-
-        struct Parameter {
-            Wrap<Model> defaultModel;
-            Wrap<Model> valueModel;
-        };
-
         using ParameterList = Collection<Model>;
 
-        struct Data : public Model {
+        const struct Nil : public Model
+        {
+            Nil() : Model("nil") {}
+            inline S8 toString() { return ""; }
+        } nil;
+
+        struct Null : public virtual Model
+        {
+            Null() : Model("null") {}
+            inline ByteSet toSet() { return ByteSet { 0 }; }
+            inline S8 toString() { return ""; }
+        };
+
+        struct Data : public virtual Model
+        {
             Address address;
             Data(S8 n, Address a = {}) : Model(n), address(a) {}
-            Data(Address a = {}) : Data{ "data", a } {}
+            Data(Address a = {}) : Data { "data", a } {}
             Address addr(U16 offset = 0)
             {
-                Address a{ address };
+                Address a { address };
                 Utility::MachineMemory::forward(a, offset);
                 return a;
             }
-            inline bool isEmpty() { return toSet().empty(); }
-            inline bool isNull() { return toSet().size() == 1 && toSet().at(0) == 0; }
+            inline S8 toString() { return "{@" + std::to_string(address.compose()) + ":" + name + "}"; }
+            inline virtual bool isEmpty() { return toSet().empty(); }
+            inline virtual bool isNull() { return isEmpty() || toSet().size() == 1 && toSet().at(0) == 0; }
+            inline virtual bool isScalar() { return true; }
+            inline virtual Ref<Data> instantiate(Data& d) { return ref(d); };
         };
+
+        struct NullableData : public Data
+        {
+            NullableData(S8 n = "", Address a = {}) : Data { "?" + n, a } {}
+            inline void setToNull() { ByteSet set; set.assign(size(), 0); fromSet(set); }
+        };
+
+        struct Composed : public virtual Data
+        {
+            Composed(S8 container, S8 content, Address a = {}) : Data { container + "<" + content + ">", a } {}
+            inline S8 toString() { auto s = toStringArray(); return "[" + Utility::Strings::join(s, ", ") + "]"; }
+            inline virtual U16 count() { return 0; }
+            inline virtual Vector<S8> toStringArray() { return {}; }
+        };
+
     }
 }
--- Juse/Types/Primitives.h
+++ Juse/Types/Primitives.h
@@ -3,19 +3,16 @@
 #include "JTypes.h"
 #include "../operations.h"
 
-namespace Juse {
-    namespace Types {
+namespace Juse
+{
+    namespace Types
+    {
         struct Boolean : public virtual Data
         {
             bool data;
-            inline ByteSet toSet()
-            {
-                if (data) {
-                    return ByteSet{ 1 };
-                }
-                return ByteSet{ 0 };
-            }
-            Boolean(bool b = false) : Data{ "boolean" }, data(b) {}
+            inline ByteSet toSet() { return data ? ByteSet { 1 } : ByteSet { 0 }; }
+            Boolean(bool b = false) : Data { "boolean" }, data(b) {}
+            inline S8 toString() { return data ? "true" : "false"; }
         };
 
         template <IsWord T>
@@ -26,11 +23,12 @@ namespace Juse {
             {
                 return word2set<T>(data);
             }
-            Word(T data) : Data{ "word" + sizeof(T) }, data{ data } {}
-            Word(ByteSet s) : Data{ "word" + sizeof(T) }, data{}
+            Word(T data) : Data { "word" + sizeof(T) }, data { data } {}
+            Word(ByteSet s) : Data { "word" + sizeof(T) }, data {}
             {
                 data = set2word(s);
             }
+            inline S8 toString() { return std::to_string(data); }
         };
 
         using Word8 = Word<U8>;
@@ -46,13 +44,14 @@ namespace Juse {
             String<T> data;
             ByteSet toSet()
             {
-                ByteSet set{};
+                ByteSet set {};
                 for (T& c : data) {
                     set.push_back(word2set(U(c)));
                 }
                 return set;
             }
-            StringModel(String<T> str) : StringType{ "string" + sizeof(T) }, data(str) {}
+            StringModel(String<T> str) : StringType { "string" + sizeof(T) }, data(str) {}
+            inline S8 toString() { return data; }
         };
 
         using StringAscii = StringModel<CH8, U8>;
@@ -67,11 +66,12 @@ namespace Juse {
             {
                 return word2set<U>(U(data));
             }
-            Integer(T data) : Data{ "integer" + sizeof(U) }, data{ data } {}
-            Integer(ByteSet s) : Data{ "integer" + sizeof(U) }, data{}
+            Integer(T data) : Data { "integer" + sizeof(U) }, data { data } {}
+            Integer(ByteSet s) : Data { "integer" + sizeof(U) }, data {}
             {
                 data = T(set2word(s));
             }
+            inline S8 toString() { return std::to_string(data); }
         };
 
         using Integer8 = Integer<I8, U8>;
@@ -79,27 +79,29 @@ namespace Juse {
         using Integer32 = Integer<I32, U32>;
         using Integer64 = Integer<I64, U64>;
 
-        struct ArrayModel : public virtual Data
+        struct ArrayModel : public virtual Composed
         {
             Collection<Data> values;
-            ArrayModel(U16 maxSize) : Data{ "array" }, values{}, maxSize(maxSize) {};
-            void add(Data& m)
-            {
-                values.push_back(m);
-            }
-            Data& at(U16 index)
-            {
-                return values.at(index);
-            }
+            ArrayModel(U16 maxSize = 0) : Composed { "array", "data" }, values {}, maxSize(maxSize) {};
+            inline void add(Data& m) { values.push_back(m); }
+            inline Data& at(U16 index) { return values.at(index); }
             ByteSet toSet()
             {
-                ByteSet set{};
+                ByteSet set {};
                 for (Data& v : values) {
                     for (U8 byte : v.toSet()) {
                         set.push_back(byte);
                     }
                 }
                 return set;
+            }
+            Vector<S8> toStringArray()
+            {
+                Vector<S8> v;
+                for (auto& s : values) {
+                    v.push_back(s.get().toString());
+                }
+                return v;
             }
         private:
             U16 maxSize;
